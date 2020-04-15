064fd2ba90518551b5a564d78b42b7514b116513
Mike <rans@email.com>
2020-04-15 06:42:06 +0200

    Update to latest HDX Python API

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 from hdx.utilities.loader import load_file_to_str
 
 requirements = ['python-slugify',
-                'hdx-python-api>=4.0.8']
+                'hdx-python-api>=4.3.9']
 
 classifiers = [
     "Development Status :: 5 - Production/Stable",
